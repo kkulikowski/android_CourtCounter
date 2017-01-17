ca63bb8fec9f42b3372eced8defb3ef0a6ebb08e
kkulikowski <kontakt@internetowakuznia.pl>
2017-01-17 22:51:12 +0100

    Team A score counter ready

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
